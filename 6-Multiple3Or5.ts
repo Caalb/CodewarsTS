10a12185065b134dcccd0a21b0fd53fbad1757ef
Caalb <carlosmunizfjunior@gmail.com>
2023-03-29 19:38:22 -0300

    feat: takes the sum of all numbers that are multiples of 3 or 5

---
+++ 6-Multiple3Or5.ts
@@ -0,0 +1,19 @@
+const solution = (num) => {
+  if(num <= 2) {
+    return 0;
+  }
+
+  let multiples = []
+
+  for(let i = 0; i < num; i++) {
+      if(i % 3 === 0 || i % 5 ===0) {
+        multiples.push(i);
+      }
+    }
+
+  const result = multiples.reduce((a,b) => a + b, 0)
+
+  return result;
+}
+
+console.log(solution(10))
